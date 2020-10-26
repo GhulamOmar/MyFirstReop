acc7582324425ac874f6aa645f05aebdc1a20342
AOmar <AOmar@192.168.0.78>
2020-10-25 21:04:51 -0500

    Second commit

--- MyFirstRepo/src/myfirstrepo.java
+++ MyFirstRepo/src/myfirstrepo.java
@@ -3,6 +3,8 @@ public class myfirstrepo {
 
 	   public static void main(String[] args) {
 	        System.out.println("Hello, World!"); 
+	        System.out.println("Test!"); 
 	        System.out.println("This is my first repo!"); 
+	        
 	    }
 	}
